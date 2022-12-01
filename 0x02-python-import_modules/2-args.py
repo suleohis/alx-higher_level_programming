49eb65ccc9931ba32168abe26890b7de57dd5678
suleohis <suleephraim1@gmail.com>
2022-12-01 22:23:04 +0100

    0x02. Python - import & modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+
+    if length == 1:
+        print("{} arguments.".format((length - 1)))
+    elif length == 2:
+        print("{} argument:".format((length - 1)))
+        for i in range(1, length):
+            print("{}: {}".format(i, sys.argv[i]))
+    else:
+        print("{} arguments:".format((length - 1)))
+        for i in range(1, length):
+            print("{}: {}".format(i, sys.argv[i]))
