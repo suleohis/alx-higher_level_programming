97d04d7d08cfda52da22cb0d2aa3b2df620bfb64
suleohis <suleephraim1@gmail.com>
2022-12-01 23:25:45 +0100

    0x02. Python - import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -22,3 +22,4 @@ if __name__ == "__main__":
         print('{} {} {} = {}'.format(a, operator, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        sys.ext(1)
