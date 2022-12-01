107d72d86919295dda49e975d8513a408e7fc688
suleohis <suleephraim1@gmail.com>
2022-12-01 21:35:09 +0100

    0x02. Python - import & modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-sum = add(a, b);
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    sum = add(a, b);
 
-print("{} + {} = {}".format(a, b, sum));
+    print("{} + {} = {}".format(1, b, sum));
