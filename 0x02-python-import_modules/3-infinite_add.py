8edfe14b776e5b65121133cf5528a8731db13381
suleohis <suleephraim1@gmail.com>
2022-12-01 22:32:49 +0100

    0x02. Python - import & modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     length = len(sys.argv)
     sums = 0
-    if length >= 2: 
+    if length >= 2:
         for i in range(1, length):
             sums = sums + int(sys.argv[i])
     print(sums)
