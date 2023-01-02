fe0e67e75c96b9c427ddb54a99107b73818a170c
suleohis <suleephraim1@gmail.com>
2023-01-02 22:07:10 +0100

    0x02. Python - import & modules

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,17 +1,45 @@
 #!/usr/bin/python3
+"""creates class Square."""
 
 
 class Rectangle:
+    """ Square class defined
+        Attributes:
+            width (int): width of rectangle
+            height (int): height of rectangle
+    """
     def __init__(self, width=0, height=0):
+     """initializes
+        Args:
+            height (int): height
+            width (int): width
+        Returns:
+            None
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """
+        getter of width
+        Return:
+            Width of Rectangle
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Setter of width
+        Args:
+            value (int): width
+        Raises
+            TypeError: if size is not int
+            ValueError: size less than 0
+        Returns:
+            None
+        """
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -21,10 +49,25 @@ class Rectangle:
 
     @property
     def height(self):
+        """
+        getter of height
+        Return:
+           height of Rectangle
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Setter of height
+        Args:
+            value (int): height
+        Raises
+            TypeError: if size is not int
+            ValueError: size less than 0
+        Returns:
+            None
+        """
         if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
